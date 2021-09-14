829eda4c9c17c2b953a3f975aa149f583f097996
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2021-09-14 16:46:45 +0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed921e2e2e6552758519a3ae35a61237'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a079ce7d-1dc8-4689-8fb2-36672531d05f'
+};
